0dfe1f701487aff5da4f841178bec79a6f83898d
Mahin B <mahinshoeb16@gmail.com>
2024-07-18 14:15:17 +0400

    changes to landing title

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -6,8 +6,8 @@ function Hero() {
   return (
     <div className='flex flex-col items-center mx-56 gap-9'>
       <h1 className='font-extrabold text-[50px] text-center mt-16'>
-        <span className='text-[#8762cd]'>Discover AI:</span> Plan out your next adventure using AI</h1>
-        <p className='text-xl text-gray-500 text-center'>Your personal AI travel planner for hotel recommendation, daily itinerary and more! </p>
+        <span className='text-[#8762cd]'>TripMate AI:</span> Discover the future of travel planning with TripMate AI, your intelligent trip companion. </h1>
+        <p className='text-xl text-gray-500 text-center'> Whether you're looking for a relaxing beach getaway, a cultural city exploration, or an adventurous mountain trek, TripMate AI simplifies the planning process by providing curated recommendations, and booking assistance. Say goodbye to travel stress and hello to seamless, smart adventures with TripMate AI. </p>
         <Link to={'/create-trip'}>
           <Button>Get Started</Button>
         </Link>
