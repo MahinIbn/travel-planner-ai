763cd9faef451e6d9773ef0bc224a70caa4a5d5f
Mahin B <mahinshoeb16@gmail.com>
2024-07-18 12:26:41 +0400

    change landing page

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -6,11 +6,12 @@ function Hero() {
   return (
     <div className='flex flex-col items-center mx-56 gap-9'>
       <h1 className='font-extrabold text-[50px] text-center mt-16'>
-        <span className='text-[#62cd80]'>Discover AI:</span> Plan your next adventure using AI</h1>
-        <p className='text-xl text-gray-500 text-center'>Your personal AI travel planner</p>
+        <span className='text-[#8762cd]'>Discover AI:</span> Plan out your next adventure using AI</h1>
+        <p className='text-xl text-gray-500 text-center'>Your personal AI travel planner for hotel recommendation, daily itinerary and more! </p>
         <Link to={'/create-trip'}>
           <Button>Get Started</Button>
         </Link>
+        <img src='/trips.png'/>
     </div>
   )
 }
